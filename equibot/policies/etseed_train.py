3c2a7c7b86aba74186a0dba965d5ddd00b3ae926
someone <you@example.com>
2025-03-20 07:18:15 +0100

    hyperparam for se3 diffusion

--- equibot/policies/etseed_train.py
+++ equibot/policies/etseed_train.py
@@ -34,6 +34,8 @@ def main(cfg):
         "weight_decay": cfg.weight_decay,
         "betas": cfg.betas,
         "eps": cfg.eps,
+        "sigma_r":cfg.sigma_r,
+        "sigma_t": cfg.sigma_t,
         "equiv_frac": cfg.equiv_frac,
         "save_freq": cfg.save_freq,
         "diffusion_steps": cfg.diffusion_steps,
@@ -87,7 +89,7 @@ def main(cfg):
         # micromamba further complicates it by not introducing proper sys envs for cmakelists
     
     nets, optimizer, lr_scheduler = init_model_and_optimizer(device,config)
-    noise_scheduler = DiffusionScheduler(num_steps=config["diffusion_steps"],mode=config["diffusion_mode"],device=device)
+    noise_scheduler = DiffusionScheduler(num_steps=config["diffusion_steps"], sigma_r=config["sigma_r"],sigma_t=config["sigma_t"],mode=config["diffusion_mode"],device=device)
     
     wandb.init(
         entity=cfg.wandb.entity,
